cb3cbefa71d903453c8eda6f02f72cd0d94419da
Annika Grothaus <annika.grothaus@denkbares.com>
2021-10-23 11:07:16 +0200

    using sklearn train_test, categorical data encoded in dummy columns, added basic classifiers

--- classifier.py
+++ classifier.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import numpy as np
 from sklearn import decomposition, tree
+from sklearn import ensemble
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+import sklearn.preprocessing
+from sklearn.model_selection import train_test_split
 import seaborn as sns
 sns.set()
 
 #TODO ausreiser in reihe 843, und irgendwo im letzten Label
+# TODO scale data
+# TODO deal with categorical data
+
+#TODO DO NOT FORGET TO PREPROC EVALUATION SET!!!!!, preProc df and preProc Matrix
 
 """Fetches training data
 
@@ -24,7 +35,6 @@ def fetch_dataset(path, details=False):
 
     #remove rows without resonable data
     df= df[pd.to_numeric(df['index'], errors='coerce').notnull()]
-
     # remove data without labels
     if(details==True):
         print("-----------------Dropping Rows--------------------\n"+ str(df[df.isnull().any(axis=1)]))
@@ -32,13 +42,15 @@ def fetch_dataset(path, details=False):
 
     # set numeric columns with missing data to nan
     data_types=['numeric','numeric', 'numeric', 'numeric','numeric', 'numeric', 'string', 'numeric', 'numeric', 'numeric', 'numeric', 'bool', 'numeric']
-    #check if there is data in numeric columns of wrong type
+    #check if there is data in numeric columns of wrong type #TODO also for evaluation?
     for i in range(0,13):
         if(data_types[i]=='numeric'):
             df["x"+str(i+1)]=pd.to_numeric(df["x"+str(i+1)], errors='coerce')
 
     if details==True:
         print (df)
+
+    df= preProcDf(df)
 
     data = df.to_numpy()
 
@@ -94,17 +106,17 @@ def testClassifier(classifier, dim=0, split=0.7, ntrials=100, plot = False):
 
     X,y = fetch_dataset("data/TrainOnMe-2.csv", details=False) ##TODO ,pcadim
 
-    X= preProc(X)
-
     pcadim=0
 
     means = np.zeros(ntrials,)
+    X=preProcNp(X)
 
     for trial in range(ntrials):
 
         #print("TRIAL", trial)
 
-        xTr,yTr,xTe,yTe,trIdx,teIdx = trteSplitEven(X,y,split,trial)
+        #xTr,yTr,xTe,yTe,trIdx,teIdx = trteSplitEven(X,y,split,trial)
+        xTr, xTe, yTr, yTe= train_test_split(X,y,test_size=0.3, random_state=42, stratify=y)
 
         # replace nan values with mean
         tr_col_mean=np.nanmean(xTr, axis=0)
@@ -143,6 +155,7 @@ def testClassifier(classifier, dim=0, split=0.7, ntrials=100, plot = False):
         plotAccuracy(means)
 
     print("Final mean classification accuracy ", "%.3g" % (np.mean(means)), "with standard deviation", "%.3g" % (np.std(means)))
+    return trained_classifier, np.mean(means), np.std(means)
 
 def plotAccuracy(means):
     indices = np.arange(1, len(means)+1)
@@ -155,29 +168,43 @@ def plotAccuracy(means):
 """ Preproc String data in feature matrix
 
     Parameters:
-    X: feature matrix
+    df: pandas dataframe
 
     Returns:
     feature matrix with strings substituted by unique integers
 
 """
-def preProc(X):
+def preProcDf(df):
 
-    #TODO handle outlier
-    #TODO handle nan
+    #handle categorical data
+    x7_dummy= pd.get_dummies(df['x7'])
+    x12_dummy= pd.get_dummies(df['x12'])
+    df=pd.concat((df,x7_dummy,x12_dummy), axis=1)
+    df=df.drop(['x7', 'x12'], axis=1)
 
-    unique_vals=np.unique(X[:,6])
-    #TODO deal with it that it should always be the same, its categorical data! (int bilded das mit intervallskala falsch ab?)
-    for idx,val in enumerate (unique_vals):
-        jdx= np.where(X[:,6]==val)[0]
-        X[jdx,6]=idx
+    # unique_vals=np.unique(X[:,6])
 
-    unique_vals=np.unique(X[:,11])
-    for idx,val in enumerate (unique_vals):
-        jdx= np.where(X[:,11]==val)[0]
-        X[jdx,11]=idx
+    # for idx,val in enumerate (unique_vals):
+    #     jdx= np.where(X[:,6]==val)[0]
+    #     X[jdx,6]=idx
+    #
+    # unique_vals=np.unique(X[:,11])
+    # for idx,val in enumerate (unique_vals):
+    #     jdx= np.where(X[:,11]==val)[0]
+    #     X[jdx,11]=idx
+    #
+
+    return df
+
+def preProcNp(X):
+    #TODO handle outlier here!
+
+    #TODO next line also needed for evaluation data?
     X = X.astype('float64')
+
     return X
+
+# -------------------------------------------------------------------------
 
 # try out different base classifiers
 class DecisionTreeClassifier(object):
@@ -197,14 +224,147 @@ class DecisionTreeClassifier(object):
     def classify(self, X):
         return self.classifier.predict(X)
 
+class RandomForestClassifier(object):
+    def __init__(self):
+        self.trained = False
 
-#TODO scale data?
+    def trainClassifier(self, Xtr, yTr, W=None):
+        rtn = RandomForestClassifier()
+        rtn.classifier = ensemble.RandomForestClassifier(n_estimators=100)
+        if W is None:
+            rtn.classifier.fit(Xtr, yTr)
+        else:
+            rtn.classifier.fit(Xtr, yTr, sample_weight=W.flatten())
+        rtn.trained = True
+        return rtn
 
-testClassifier(DecisionTreeClassifier(),split=0.7, plot=True)
+    def classify(self, X):
+        return self.classifier.predict(X)
+
+
+class GradientBoostingClassifier(object):
+    def __init__(self):
+        self.trained = False
+
+    def trainClassifier(self, Xtr, yTr, W=None):
+        rtn = GradientBoostingClassifier()
+        rtn.classifier = ensemble.GradientBoostingClassifier(n_estimators=50)
+        if W is None:
+            rtn.classifier.fit(Xtr, yTr)
+        else:
+            rtn.classifier.fit(Xtr, yTr, sample_weight=W.flatten())
+        rtn.trained = True
+        return rtn
+
+    def classify(self, X):
+        return self.classifier.predict(X)
+
+class NaiveBayesClassifier(object):
+    def __init__(self):
+        self.trained = False
+
+    def trainClassifier(self, Xtr, yTr, W=None):
+        rtn = NaiveBayesClassifier()
+        rtn.classifier = GaussianNB()
+        if W is None:
+            rtn.classifier.fit(Xtr, yTr)
+        else:
+            rtn.classifier.fit(Xtr, yTr, sample_weight=W.flatten())
+        rtn.trained = True
+        return rtn
+
+    def classify(self, X):
+        return self.classifier.predict(X)
+
+
+class KNeiClassifier(object):
+    def __init__(self):
+        self.trained = False
+
+    def trainClassifier(self, Xtr, yTr, W=None):
+        rtn = KNeiClassifier()
+        rtn.classifier = KNeighborsClassifier(n_neighbors=3)
+        if W is None:
+            rtn.classifier.fit(Xtr, yTr)
+        else:
+            rtn.classifier.fit(Xtr, yTr, sample_weight=W.flatten())
+        rtn.trained = True
+        return rtn
+
+    def classify(self, X):
+        return self.classifier.predict(X)
+
+
+class LogisticRegressionClassifier(object):
+    def __init__(self):
+        self.trained = False
+
+    def trainClassifier(self, Xtr, yTr, W=None):
+        rtn = LogisticRegressionClassifier()
+
+        Xtr= sklearn.preprocessing.scale(Xtr)
+        rtn.classifier = LogisticRegression(max_iter=100000000)
+        if W is None:
+            rtn.classifier.fit(Xtr, yTr)
+        else:
+            rtn.classifier.fit(Xtr, yTr, sample_weight=W.flatten())
+        rtn.trained = True
+        return rtn
+
+    def classify(self, X):
+        return self.classifier.predict(X)
+
+class SVMLinClassifier(object):
+    def __init__(self):
+        self.trained = False
+
+    def trainClassifier(self, Xtr, yTr, W=None):
+        rtn = SVMLinClassifier()
+        rtn.classifier = SVC(probability=True, kernel="linear")
+        if W is None:
+            rtn.classifier.fit(Xtr, yTr)
+        else:
+            rtn.classifier.fit(Xtr, yTr, sample_weight=W.flatten())
+        rtn.trained = True
+        return rtn
+
+    def classify(self, X):
+        return self.classifier.predict(X)
+
+class SVMDefClassifier(object):
+    def __init__(self):
+        self.trained = False
+
+    def trainClassifier(self, Xtr, yTr, W=None):
+        rtn = SVMDefClassifier()
+        rtn.classifier = SVC(probability=True)
+        if W is None:
+            rtn.classifier.fit(Xtr, yTr)
+        else:
+            rtn.classifier.fit(Xtr, yTr, sample_weight=W.flatten())
+        rtn.trained = True
+        return rtn
+
+    def classify(self, X):
+        return self.classifier.predict(X)
+
+
+
+scores=[]
+
+#scores.append(testClassifier(DecisionTreeClassifier(),split=0.7, plot=True))
+scores.append(testClassifier(RandomForestClassifier(),split=0.7, plot=True))
+#scores.append(testClassifier(GradientBoostingClassifier(),split=0.7, plot=True))
+
+#scores.append(testClassifier(NaiveBayesClassifier(),split=0.7, plot=True))
+#scores.append(testClassifier(KNeiClassifier(),split=0.7, plot=True))
+#scores.append(testClassifier(LogisticRegressionClassifier(),split=0.7, plot=True))
+#scores.append(testClassifier(SVMLinClassifier(),split=0.7, plot=True))
+#scores.append(testClassifier(SVMDefClassifier(),split=0.7, plot=True))
+scores=scores
 
 
 
 
-
-
+print(scores)
 
