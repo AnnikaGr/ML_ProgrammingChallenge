1189efe079ee3af731bcf581dfad14dac0a96d0f
Annika Grothaus <annika.grothaus@denkbares.com>
2021-10-27 20:59:20 +0200

    Final mean classification accuracy  60.7 with standard deviation 1.1

--- classifier.py
+++ classifier.py
@@ -1,6 +1,8 @@
+from statistics import variance
+
 import pandas as pd
 import numpy as np
-from sklearn import decomposition, tree
+from sklearn import decomposition, tree, preprocessing
 from sklearn import ensemble
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
@@ -8,18 +10,35 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 import sklearn.preprocessing
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, RandomizedSearchCV
 import seaborn as sns
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.over_sampling import SMOTENC
+from imblearn.over_sampling import ADASYN
+from imblearn.pipeline import Pipeline
+
 sns.set()
 
-# TODO handle outliers, ausreiser in reihe 843, und irgendwo im letzten Label
+# TODO handle outliers, ausreiser in reihe 843, und irgendwo im letzten Label x11-55, x4-851
 # TODO scale data
-# TODO deal with categorical data differently, research names connections
+# TODO maybe: research names connections
 # TODO research on classifiying mixed data
-#TODO AUC
-# TODO check overfitting/hyperparametersearch
+# TODO AUC
 
-#TODO DO NOT FORGET TO PREPROC EVALUATION SET!!!!!, preProc df and preProc Matrix
+# TODO check overfitting/hyperparametersearch
+# TODO https://towardsdatascience.com/boost-machine-learning-performance-by-30-with-normalization-and-standardization-156adfbf215b
+# TODO https://towardsdatascience.com/machine-learning-multiclass-classification-with-imbalanced-data-set-29f6a177c1a
+
+#TODO is this dataset inbalanced?
+# TODO oversampling for atsuto, So we should balance our dataset before training our classifier. There are various methods to do this, including subsampling (taking a smaller yet equal selection of samples from each class), upsampling (taking repeat samples from some classes to increase its numbers), resampling (using an algorithm like SMOTE to augment the dataset with artificial data), etc
+# https://medium.com/@b.terryjack/tips-and-tricks-for-multi-class-classification-c184ae1c8ffc
+
+#TODO DO NOT FORGET TO PREPROC EVALUATION SET!!!!!, preProcDf AND preProc Matrix
 
 """Fetches training data
 
@@ -54,14 +73,22 @@ def fetch_dataset(path, details=False):
         print (df)
 
     df= preProcDf(df)
-
-
+    if details==True:
+        for i in range(2, len(df.iloc[0])):
+            col= df.iloc[:,i]
+            plt.figure();
+            col.plot(kind="hist");
+            plt.xlabel(df.columns[i])
+            plt.autoscale(enable=True, axis='both', tight=True)
+            plt.show()
 
     data = df.to_numpy()
-
     y=data[:,1]
-    X=data[:,2:15]
+    X=data[:,2:] # TODO gut wenn nur bis 12 mit hot encoding (alle categorical rauslassen)
 
+    for i in range(len(X[0])):
+        tmp=X[:,i]
+        print ("Variance column "+ str(i) + ": " + str(variance(X[:,i])))
     return X,y
 
 """ Train-Test split
@@ -121,7 +148,7 @@ def testClassifier(classifier, dim=0, split=0.7, ntrials=100, plot = False):
         #print("TRIAL", trial)
 
         #xTr,yTr,xTe,yTe,trIdx,teIdx = trteSplitEven(X,y,split,trial)
-        xTr, xTe, yTr, yTe= train_test_split(X,y,test_size=0.3, random_state=42, stratify=y)
+        xTr, xTe, yTr, yTe= train_test_split(X,y,test_size=0.3,random_state=42) #,random_state=42
 
         # replace nan values with mean
         tr_col_mean=np.nanmean(xTr, axis=0)
@@ -133,6 +160,27 @@ def testClassifier(classifier, dim=0, split=0.7, ntrials=100, plot = False):
         ids= np.where(np.isnan(xTe))
         if(len(ids)!=0):
             xTe[ids]= np.take(te_col_mean, ids[1])
+
+        # oversample minority class
+        #print("lenght yTr:" + str(len(xTr)))
+
+        # print('Atsuto: '+ str(len(np.where(yTr=='Atsuto')[0])))
+        # print('Jorg: '+ str(len(np.where(yTr=='Jorg')[0])))
+        # print('Shoogee: '+ str(len(np.where(yTr=='Shoogee')[0])))
+        # print('Bob: '+ str(len(np.where(yTr=='Bob')[0])))
+        #print("Before oversampling -----------------------------------------------")
+        #classifier.searchHyperparameters(xTr, xTe, yTr, yTe)
+
+        dic={'Atsuto':150}
+        #cat_mask=[False, False,False,False,False, False, False,False,False,False, False, True,True,True,True,True]
+        oversample = RandomOverSampler(sampling_strategy='not majority')
+        xTr, yTr = oversample.fit_resample(xTr, yTr)
+
+        # print('Atsuto: '+ str(len(np.where(yTr=='Atsuto')[0])))
+        # print('Jorg: '+ str(len(np.where(yTr=='Jorg')[0])))
+        # print('Shoogee: '+ str(len(np.where(yTr=='Shoogee')[0])))
+        # print('Bob: '+ str(len(np.where(yTr=='Bob')[0])))
+
 
         # Do PCA replace default value if user provides it
         if dim > 0:
@@ -182,26 +230,58 @@ def plotAccuracy(means):
 def preProcDf(df):
 
     #handle categorical data
-    x7_dummy= pd.get_dummies(df['x7'])
-    x12_dummy= pd.get_dummies(df['x12'])
-    x12_dummy.describe()
-    x12_dummy.columns = ['False','True']
-    x7_dummy.describe()
-    x7_dummy.columns = ['name1','name2', 'name3', 'name4', 'name5']
-    df=pd.concat((df,x7_dummy,x12_dummy), axis=1)
-    df=df.drop(['x7', 'x12'], axis=1)
-    df=df.drop(['False'], axis=1)
-    df=df.drop(['name5'], axis=1)
+    # x7_dummy= pd.get_dummies(df['x7'])
+    # x12_dummy= pd.get_dummies(df['x12'])
+    # x12_dummy.describe()
+    # x12_dummy.columns = ['False','True']
+    # x7_dummy.describe()
+    # x7_dummy.columns = ['name1','name2', 'name3', 'name4', 'name5']
+    # df=pd.concat((df,x7_dummy,x12_dummy), axis=1)
+    # df=df.drop(['x7', 'x12'], axis=1)
+    # df=df.drop(['False'], axis=1)
+    # df=df.drop(['name5'], axis=1)
+
+    le = preprocessing.LabelEncoder()
+    le.fit(df['x7'])
+    df['x7']=le.transform(df['x7'])
+
+    le = preprocessing.LabelEncoder()
+    le.fit(df['x12'])
+    df['x12']=le.transform(df['x12'])
+
+    df.drop(55, inplace=True)
+    df.drop(851, inplace=True)
 
     return df
 
 def preProcNp(X):
-    #TODO handle outlier here!
+    #TODO handle outlier here! RobustScaler? Why is mean wit
 
     #TODO next line also needed for evaluation data?
+    #print("Before: "+ str(X.mean(axis=0)))
     X = X.astype('float64')
+    # transformer = StandardScaler() #RobustScaler()
+    # transformer.fit(X)
+    # X= transformer.transform(X)
+    # #X= transformer.fit_transform(X)
 
+    #print("After: "+ str(X.mean(axis=0)))
     return X
+
+def searchHyper(classifier):
+    X,Y = fetch_dataset("data/TrainOnMe-2.csv", details=False) ##TODO ,pcadim
+
+    X=preProcNp(X)
+
+    # replace nan values with mean
+    tr_col_mean=np.nanmean(X, axis=0)
+    ids= np.where(np.isnan(X))
+    if (len(ids)!=0):
+        X[ids] = np.take(tr_col_mean, ids[1])
+
+    # oversample = RandomOverSampler(sampling_strategy='not majority')
+    # X, Y = oversample.fit_resample(X, Y)
+    classifier.searchHyperparameters(X, Y)
 
 # -------------------------------------------------------------------------
 
@@ -212,6 +292,8 @@ class DecisionTreeClassifier(object):
 
     def trainClassifier(self, Xtr, yTr, W=None):
         rtn = DecisionTreeClassifier()
+
+
         rtn.classifier = tree.DecisionTreeClassifier(max_depth=Xtr.shape[1]/2+1)
         if W is None:
             rtn.classifier.fit(Xtr, yTr)
@@ -227,9 +309,62 @@ class RandomForestClassifier(object):
     def __init__(self):
         self.trained = False
 
+    def searchHyperparameters(self, XTr, xTe, yTr, yTe):
+        rtn = RandomForestClassifier()
+
+        model = Pipeline([
+            ('sampling', RandomOverSampler()),
+            ('classification', ensemble.RandomForestClassifier())
+        ])
+
+        #forest =ensemble.RandomForestClassifier(n_estimators=400, max_depth=50)
+        parameter_space = {
+            # 'classification__n_estimators': [1000],
+            'classification__max_depth': [None,6,15,30,200],
+            'classification__min_samples_split': [2,20,50,200],
+            # 'classification__max_leaf_nodes': [None, 5, 20,100],
+            'classification__min_samples_leaf': [1, 20,50,100],
+            'classification__max_samples': [None, 0.2, 0.5, 0.7],
+            'classification__max_features': [None, 'sqrt', 0.5, 0.6, 0.7 ,0.8,0.9]
+        }
+
+        # parameter_space = {
+        #     'classification__n_estimators': [1000],
+        #     'classification__max_depth': [None, 4,6,9,11,15,20,30,200],
+        #     'classification__min_samples_split': [2,20,50,200],
+        #     'classification__max_leaf_nodes': [None, 5,10, 20,100],
+        #     'classification__min_samples_leaf': [1, 10,20,50,100,300],
+        #     'classification__max_samples': [None, 0.2, 0.5, 0.7],
+        # }
+
+
+        # define evaluation
+        cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+        # define search
+        #clf = GridSearchCV(estimator=model, param_grid=parameter_space, n_jobs=-1, verbose=1, cv=5, scoring="accuracy")
+        clf=RandomizedSearchCV(model, param_distributions=parameter_space, n_iter=500, scoring='accuracy', n_jobs=-1, cv=cv, random_state=1)
+        clf.fit(XTr, yTr)
+
+
+        # Best paramete set
+        print('Best parameters found:\n', clf.best_params_)
+
+        # All results
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+
+        print("Best estimator and their score: ")
+        print(clf.best_estimator_)
+        print(clf.score(xTe, yTe))
+
+
     def trainClassifier(self, Xtr, yTr, W=None):
         rtn = RandomForestClassifier()
-        rtn.classifier = ensemble.RandomForestClassifier(n_estimators=100)
+
+        rtn.classifier =ensemble.RandomForestClassifier(n_estimators=1000, max_depth=Xtr.shape[1]/2+1, max_features=0.5)
+
         if W is None:
             rtn.classifier.fit(Xtr, yTr)
         else:
@@ -247,7 +382,7 @@ class GradientBoostingClassifier(object):
 
     def trainClassifier(self, Xtr, yTr, W=None):
         rtn = GradientBoostingClassifier()
-        rtn.classifier = ensemble.GradientBoostingClassifier(n_estimators=50)
+        rtn.classifier = ensemble.GradientBoostingClassifier(n_estimators=100)
         if W is None:
             rtn.classifier.fit(Xtr, yTr)
         else:
@@ -348,18 +483,81 @@ class SVMDefClassifier(object):
         return self.classifier.predict(X)
 
 
+class NNClassifier(object):
+    def __init__(self):
+        self.trained = False
+
+    def trainClassifier(self, Xtr, yTr, W=None):
+        mlp = MLPClassifier(max_iter=10000)
+        parameter_space = {
+            'hidden_layer_sizes': [(50,50,50), (50,100,50), (100,)],
+            'activation': ['tanh', 'relu'],
+            'solver': ['sgd', 'adam'],
+            'alpha': [0.0001, 0.05],
+            'learning_rate': ['constant','adaptive'],
+        }
+
+        clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=3)
+        clf.fit(Xtr, yTr)
+
+        # Best paramete set
+        print('Best parameters found:\n', clf.best_params_)
+
+        # All results
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+
+        rtn = NNClassifier()
+        rtn.classifier= clf
+        #rtn.classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1, max_iter=10000)
+        if W is None:
+            rtn.classifier.fit(Xtr, yTr)
+        else:
+            rtn.classifier.fit(Xtr, yTr, sample_weight=W.flatten())
+        rtn.trained = True
+        return rtn
+
+    def classify(self, X):
+        return self.classifier.predict(X)
+
+
+class StockGradDesClassifier(object):
+    def __init__(self):
+        self.trained = False
+
+    def trainClassifier(self, Xtr, yTr, W=None):
+        rtn = StockGradDesClassifier()
+        rtn.classifier = SGDClassifier(loss="hinge", penalty="l2", max_iter=5)
+        if W is None:
+            rtn.classifier.fit(Xtr, yTr)
+        else:
+            rtn.classifier.fit(Xtr, yTr, sample_weight=W.flatten())
+        rtn.trained = True
+        return rtn
+
+    def classify(self, X):
+        return self.classifier.predict(X)
+
+
 
 scores=[]
 
 #scores.append(testClassifier(DecisionTreeClassifier(),split=0.7, plot=True))
 #scores.append(testClassifier(RandomForestClassifier(),split=0.7, plot=True))
-scores.append(testClassifier(GradientBoostingClassifier(),split=0.7, plot=True))
+#scores.append(testClassifier(GradientBoostingClassifier(),split=0.7, plot=True))
 
 #scores.append(testClassifier(NaiveBayesClassifier(),split=0.7, plot=True))
 #scores.append(testClassifier(KNeiClassifier(),split=0.7, plot=True))
 #scores.append(testClassifier(LogisticRegressionClassifier(),split=0.7, plot=True))
 #scores.append(testClassifier(SVMLinClassifier(),split=0.7, plot=True))
 #scores.append(testClassifier(SVMDefClassifier(),split=0.7, plot=True))
+
+
+testClassifier(RandomForestClassifier(),split=0.7, plot=False, ntrials=100)
+#searchHyper(RandomForestClassifier())
+
 scores=scores
 
 
